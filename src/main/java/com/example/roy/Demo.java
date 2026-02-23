4f990e1fe2c2979835d52dd7e55e3fc2e13a0391
royder789 <kumarmihir798@gmail.com>
2026-02-23 15:02:27 +0530

    commit

---
+++ src/main/java/com/example/roy/Demo.java
@@ -0,0 +1,19 @@
+package com.example.roy;
+
+public class Demo {
+    public static void greetMorning() {
+        System.out.println("Good Morning!");
+    }
+
+    public static void greetAfternoon() {
+        System.out.println("Good Afternoon!");
+    }
+
+    public static void greetEvening() {
+        System.out.println("Good Evening!");
+    }
+
+    public static void greetNight() {
+        System.out.println("Good Night!");
+    }
+}
